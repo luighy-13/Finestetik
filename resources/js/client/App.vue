5f0c3bf4d2b7eeb7f2f5539d89d92b750f7facf7
desarrolloAs <luciano_flores@abastossuper.com>
2022-06-15 16:16:13 -0500

    update

--- resources/js/client/App.vue
+++ resources/js/client/App.vue
@@ -1,8 +1,10 @@
 <template>
-    <div>
-        <div class="bg-primary w-full h-3/6">
+    <div class="">
+        <div class="bg-primary w-full h-3/6 border-bottom">
             <div class="flex h-20">
-                <div class="w-1/2"></div>
+                <div class="w-1/2 relative">
+                    <img src="/logo-white.png" class="" />
+                </div>
                 <div class="w-1/2 flex flex-row items-center justify-end">
                     <a class="font-bold mr-2">Inicio</a>
                     <a class="font-bold mr-2">Planes</a>
@@ -13,7 +15,9 @@
             </div>
         </div>
 
-        <div class=" bk-presentation h-screen flex-center flex-column w-full items-center justify-end">
+        <div
+            class="bk-presentation flex-center flex-column w-full items-center justify-end"
+        >
             <p class="text-center text-4xl font-bold">
                 Planes de financiamiento para tu cirugía estética
             </p>
@@ -23,17 +27,51 @@
             </p>
         </div>
 
-        <div >
-            <p class="text-center text-2xl font-bold">¿Quiénes somo?</p>
-            <p class="w-1/2 text-center m-auto">
-                "Somos un grupo de emprendedores en el sector de financiamiento
-                especializado. Nos enfocamos en ofrecer planes de financiamiento
-                sencillos de entender, fácil y rápido de obtener y que mejor se
-                acomoden a tus necesidades."
-            </p>
+        <div class="mt-8">
+                <p class="text-center text-2xl font-bold">¿Quiénes somos?</p>
+                <p class="w-1/2 text-center m-auto italic font-medium">
+                    "Somos un grupo de emprendedores en el sector de
+                    financiamiento especializado. Nos enfocamos en ofrecer
+                    planes de financiamiento sencillos de entender, fácil y
+                    rápido de obtener y que mejor se acomoden a tus
+                    necesidades."
+                </p>
         </div>
 
+        <div class="mt-8">
+            <div class="w-full text-center">
+                <p class="text-2xl font-bold">Contactanos</p>
+                <p>Un asesor se pondrá en contacto con usted</p>
+            </div>
+            <div class="flex">
+                <div class="w-1/2"></div>
+                <div class="w-1/2 m-2">
+                    <form cass>
+                        <div class="flex">
+                            <div class="w-1/2">
+                                <label>Nombre</label>
+                                <input class type="text" />
+                            </div>
+                            <div class="w-1/2">
+                                <label>Teléfono</label>
+                                <input class type="email" />
+                            </div>
+                        </div>
+                        <div>
+                            <label>Correo electrónico </label>
+                            <input class type="flex-1 email" />
+                        </div>
+                        <label>Mensaje</label>
+                        <textarea></textarea>
+                    </form>
+                </div>
+            </div>
+        </div>
 
-
+        <div class="bg-primary w-full h-3/6 ">
+            <p class="w-full text-center font-bold text-1xl uppercase">
+                finestetik © 2022
+            </p>
+        </div>
     </div>
 </template>
