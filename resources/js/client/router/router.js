4358d6159cf206ee1ef5b399fee400c406210aaf
Luciano Flores A <lucianofloresacosta.13@gmail.com>
2022-06-28 14:02:56 -0500

    admin and routers view

--- resources/js/client/router/router.js
+++ resources/js/client/router/router.js
@@ -1,17 +1,35 @@
-// just do this:
-import Vue from 'vue'; //import again even though you already imported it
-import Router from 'vue-router'; // and this is where difference comes in
-import Home from "../views/home/home.vue";
+import Vue from "vue";
+import Router from 'vue-router';
+
+
+// // just do this:
+// import Vue from 'vue'; //import again even though you already imported it
+// import Router from 'vue-router'; // and this is where difference comes in
+import home from "../views/home/home.vue";
+import sing_up from '../views/sing_up/sing_up.vue'
 
 Vue.use(Router);
 const routes = [
     {
-        path: "/",
-        component: Home
+        path: "/login",
+        component: home
     },
-];
+    {
+        path: "/",
+        component: home,
+        children:[
+            {
+                path: "/sing-up", 
+                component: sing_up
+            },
+        ]
+    },
+    
 
-//then you're router to export as follows
+];
 const router = new Router({routes : routes});
+
+// //then you're router to export as follows
+// const router = new Router({routes : routes});
 
 export default router
