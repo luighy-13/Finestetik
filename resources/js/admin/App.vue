dfa0f9b1a888b8026c6e7c8fe590360f76e3f8f3
desarrolloAs <luciano_flores@abastossuper.com>
2022-08-01 15:07:43 -0500

    multiples cambios

--- resources/js/admin/App.vue
+++ resources/js/admin/App.vue
@@ -1,5 +1,6 @@
 <template>
     <div>
+        <div class="bg-rose-600 text-center text-white font-bold pt-2 pb-2">Esta página es una versión de prueba.</div>
         <router-view></router-view>
     </div>
 </template>
