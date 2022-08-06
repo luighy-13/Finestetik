1c0d110f4e403fdd2bd353ee11ebd680a461be6e
desarrolloAs <luciano_flores@abastossuper.com>
2022-08-06 13:36:12 -0500

    build letter

--- resources/js/client/App.vue
+++ resources/js/client/App.vue
@@ -1,3 +1,8 @@
 <template>
-    <router-view></router-view>
-</template
+    <div>
+        <router-view></router-view>
+        <div class="absolute bottom-0 right-0 pr-2 pl-2 bg-sky-700 font-bold text-white rounded-t">
+            Build: 0.0.1
+        </div>
+    </div>
+</template>
